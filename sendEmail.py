740c107df9702c037519e77cb906675328ae6091
EC2 Default User <ec2-user@ip-172-31-14-101.ap-southeast-2.compute.internal>
2019-09-13 22:58:47 +0000

    changed

--- sendEmail.py
+++ sendEmail.py
@@ -1,6 +1,16 @@
 import boto3
+import config
 
-sns = boto3.client('sns')
+accessKeyId= config.accessKeyId
+secretAccessKey = config.secretAccessKey
+print(accessKeyId)
+
+sns = boto3.client(
+            'sns', 
+            region_name = "ap-southeast-2", 
+            aws_access_key_id = accessKeyId, 
+            aws_secret_access_key = secretAccessKey
+        )
 
 # Publish a simple message to the specified SNS topic
 def sendEmail(price, link):
