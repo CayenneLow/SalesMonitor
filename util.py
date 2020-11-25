27d09d2d7599d0bd42eed01328bbf42d1867d9d6
CayenneLow <lowkhyeean@gmail.com>
2020-11-25 09:38:33 +0000

    added out of stock logic

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 
 def scrape(page, name, lowerBound, upperBound):
     soup = BeautifulSoup(page.data, 'html.parser')
@@ -11,7 +12,27 @@ def scrape(page, name, lowerBound, upperBound):
                 price = float(price)
                 if price >= lowerBound and price <= upperBound:
                     link = "http://www.staticice.com.au" + anchorTag['href']
-                    print(link)
-                    # sendEmail(name, price, link)
+                    if (not isOutOfStock(getPage(link))):
+                        print(link)
+                        # sendEmail(name, price, link)
             except Exception as e:
                 print(e)
+
+def getPage(link):
+    # Get redirection page
+    page = requests.get(link).text
+    soup = BeautifulSoup(page, 'html.parser')
+    metaTag = soup.find("meta")
+    redirect = metaTag["content"].split("=")[1]
+    # Access redirect page
+    html = requests.get(redirect).text
+    return html
+
+def isOutOfStock(page):
+    page = page.lower()
+    keywords = ["Out of Stock", "Notify me", "Pre Order", "Pre-Order", "Sold Out", "Discontinued"]
+    for keyword in keywords:
+        keyword = keyword.lower()
+        if keyword in page:
+            return True
+    return False
