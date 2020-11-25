39227b6817d846b253a2740aa4d3db0a31f55eef
Khye <lowkhyeean@gmail.com>
2020-11-25 21:46:59 +1100

    added lambda handler

--- app.py
+++ app.py
@@ -4,21 +4,22 @@ import requests
 import json
 import datetime
 
-# open file
-f = open("links.json", "r")
-contents = f.read()
-parsedContent = json.loads(contents)
+def lambda_handler():
+    # open file
+    f = open("links.json", "r")
+    contents = f.read()
+    parsedContent = json.loads(contents)
 
-print("=== Executing now at: {} ===".format(datetime.datetime.now()))
-for link in parsedContent:
-    try:
-        page = requests.get(link['link'], timeout=10).text  # timeout after 10 seconds
-        if "blocked" in page:
-            raise Exception("Current ip address blocked")
-        print("Scraping for {}".format(link['name']))
-        scrape(page, link['name'], float(link['lowerBound']), float(link['upperBound']))
-    except (requests.Timeout, Exception) as e:
-        reboot()
-        exit()
+    print("=== Executing now at: {} ===".format(datetime.datetime.now()))
+    for link in parsedContent:
+        try:
+            page = requests.get(link['link'], timeout=10).text  # timeout after 10 seconds
+            if "blocked" in page:
+                raise Exception("Current ip address blocked")
+            print("Scraping for {}".format(link['name']))
+            scrape(page, link['name'], float(link['lowerBound']), float(link['upperBound']))
+        except (requests.Timeout, Exception) as e:
+            reboot()
+            exit()
 
 
