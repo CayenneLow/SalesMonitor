7331377cae91f3840e88ee4ef12bedfdb8335337
Khye Ean Low <lowkhyeean@gmail.com>
2020-11-28 23:38:08 +1100

    fixed zipScript AGAIN

--- zipScript.sh
+++ zipScript.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 pip3 install --system --target ./package -r requirements.txt
-zip -r upload.zip ./package/* *.py links.json
+cd package
+zip -r upload.zip ./* 
+mv ./upload.zip ../
+cd ..
+zip upload.zip *.py links.json
